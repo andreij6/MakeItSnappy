e58951b363545f19580daac83086e8f4d5f38723
Andre Jones <andreij6@gmail.com>
2013-10-14 23:59:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeitsnappy::Application.config.secret_token = '2c2141d4918be672828937273a450f4e32713e5b41cb7b4c14d17eec68da8fe2ae79cd2cd4f424bde1e57e6d78441840da3f2b235707774861f47579a7978191'
